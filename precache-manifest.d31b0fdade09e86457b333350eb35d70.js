a68e9845e3ea3f3f2d28b31327c362016ecc08ee
unknown <konradryczko1@gmail.com>
2020-06-22 14:37:15 +0200

    Updates

---
+++ precache-manifest.d31b0fdade09e86457b333350eb35d70.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd40f55341a70375eb6556a7dce59646",
+    "url": "/Burger-app/index.html"
+  },
+  {
+    "revision": "de261442c4de557b703f",
+    "url": "/Burger-app/static/css/main.b3f50ebf.chunk.css"
+  },
+  {
+    "revision": "d6a507754d60a8dfb55f",
+    "url": "/Burger-app/static/js/2.af5c78c2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Burger-app/static/js/2.af5c78c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de261442c4de557b703f",
+    "url": "/Burger-app/static/js/main.9ba3b8d4.chunk.js"
+  },
+  {
+    "revision": "3837b555641fb234d9ba",
+    "url": "/Burger-app/static/js/runtime-main.e736c02e.js"
+  },
+  {
+    "revision": "74e261aaa5c735627c54c5596babedc7",
+    "url": "/Burger-app/static/media/fontello.74e261aa.eot"
+  },
+  {
+    "revision": "88484677d61559dd1b84df6a552655e9",
+    "url": "/Burger-app/static/media/fontello.88484677.ttf"
+  },
+  {
+    "revision": "a258810502140b649f62f168a83dc472",
+    "url": "/Burger-app/static/media/fontello.a2588105.woff2"
+  },
+  {
+    "revision": "b54067db58320914cef79b0618be366d",
+    "url": "/Burger-app/static/media/fontello.b54067db.svg"
+  },
+  {
+    "revision": "e00aa78a563e616ba3bc6069ef45a49e",
+    "url": "/Burger-app/static/media/fontello.e00aa78a.woff"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-app/static/media/logo.b8503d26.png"
+  }
+]);
